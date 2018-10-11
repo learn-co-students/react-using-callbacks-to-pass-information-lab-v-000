9414e6d5c7460aed3cb030036bef603bc76e03ab
Courtney <chalt0319@gmail.com>
2018-10-11 11:16:45 -0400

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+// import Matrix from './Matrix.js'
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.color
     }
   }
-  
+  changeColor = () => {
+    this.setState({
+      color: this.props.update()
+    })
+
+  }
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" onClick={() => this.setState({color: this.props.update})} style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
-  
+
 }
--- src/Matrix.js
+++ src/Matrix.js
@@ -7,21 +7,29 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      color: '#FFF'
+    }
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} update={this.state.color}/>)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
+  genColor = (newColor) => {
+    this.setState({
+      color: newColor
+    })
+  }
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector generate={this.genColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
