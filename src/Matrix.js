8fe428a646a40234b2cf1addaef7adee06d422c0
jadamduff <j.adam.duff@gmail.com>
2018-11-28 15:43:21 -0600

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -5,23 +5,36 @@ import ColorSelector from './ColorSelector.js'
 
 export default class Matrix extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentBg: this.props.values
+    }
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={this.props.values} getCurrentBg={this.getCurrentBg} />)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
+  getCurrentBg = () => {
+    return this.state.currentBg;
+  }
+
+  updateCurrentBg = (cellColor) => {
+    this.setState({
+      currentBg: cellColor
+    })
+  }
+
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector updateCurrentBg={this.updateCurrentBg}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
