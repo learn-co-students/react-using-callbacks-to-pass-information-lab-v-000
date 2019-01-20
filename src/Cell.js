a0346895191286c574d1ccd049ad6194b29e41da
github username <github email address>
2019-01-20 12:46:45 -0500

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -8,12 +8,25 @@ export default class Cell extends Component {
       color: this.props.color
     }
   }
+
+  handleClick = () => {
+    this.setState({
+      color: this.props.accessColorChoice()
+    })
+  }
   
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
       </div>
     )
   }
   
 }
+
+{/*
+// When I render each Cell component in genRow method in Matrix.js with the code
+// <Cell key={idx} color={val} accessColorChoice={this.accessColorChoice} />
+// I pass in the following object as its props:
+// {color: string hex, accessColorChoice: accessColorChoice callback function object}
+*/}
