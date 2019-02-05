fc30a86714d6596d47837e77c31428f71aa3f2de
netzay <netzelally@gmail.com>
2019-02-05 14:06:03 -0700

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 
 export default class Cell extends Component {
   
+
   constructor(props) {
+
     super(props)
     this.state = {
-      color: this.props.color
+      selectedColor: this.props.color
     }
   }
-  
+
+  changeColor = () => {
+    debugger
+    //setState takes in an argument, which is why the parenthesis are on the outside of the javascript on the inside
+    this.setState( {
+      selectedColor: this.props.getSelectedColor()
+    })
+  }
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      //changeColor actually changes the cell's color through the propert of state's 'selectedColor'
+      //backgroundColor changes automatically as according to whatever state is
+      <div className="cell" onClick={this.changeColor} style={{backgroundColor: this.state.selectedColor}}>
       </div>
     )
   }
-  
 }
