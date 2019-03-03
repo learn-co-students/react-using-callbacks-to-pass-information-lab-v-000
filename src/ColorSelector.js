30795042660a7510863caef1d7270808388f17fc
github username <github email address>
2019-03-03 18:32:22 -0500

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
-  
+
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      // const temp2 = str
+      // const temp3 = this.props
+      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={() => this.handleSwatchClick(str)}/>
     })
   )
-  
+
+  handleSwatchClick = (str) => {
+    const temp3 = this
+    const newColor = this.props.setNewColor(str)
+  }
+
+
+
   render() {
     return (
-      <div id="colorSelector">
+      <div id="colorSelector" >
         {this.makeColorSwatches()}
       </div>
     )
   }
-  
+
 }
