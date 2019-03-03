4213c8357a1741a3fc578d8ac67021681b3386b2
ElleeB <laura.trager@gmail.com>
2019-03-03 11:14:36 -0500

    Refactor click response to log selection's background color

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -4,12 +4,12 @@ export default class ColorSelector extends Component {
 
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={this.handleClick}/>
+      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={ (e) => this.setColor(str) }/>
     })
   )
 
-  handleClick = (event) => {
-    console.log("pow!")
+  setColor = (color) => {
+    console.log(color)
   }
 
   render() {
