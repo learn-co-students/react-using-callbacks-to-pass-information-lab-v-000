d0a56891a1e942cd3f6a1770765742dfc520e4ea
github username <github email address>
2019-03-30 13:10:02 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -3,25 +3,43 @@ import chromeBoi from './data.js'
 import Cell from './Cell.js'
 import ColorSelector from './ColorSelector.js'
 
+// provide a way for Matrix to keep track of the selected color (think state!)
+// write a method that takes in a single argument of a hex color string (i.e. '#fff') and sets the selected color to that
+// consider what should be done with that method once it is written. Who needs it? How can we get it to them? Use the skills you already have with React to solve this
+
 export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state =
+     {
+      color: '#FFF'
+     }
   }
 
+  setSelectedColor = (newColor) => {
+    this.setState({
+     color: newColor
+   })
+
+  }
+
+getSelectedColor = () => (this.state.color)
+
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} getSelectedColor={this.getSelectedColor} />)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
+  // did you make sure to pass a function as a prop to Cell instead of the selected value itself?
 
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector setSelectedColor = {this.setSelectedColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
@@ -34,3 +52,5 @@ export default class Matrix extends Component {
 Matrix.defaultProps = {
   values: chromeBoi
 }
+
+// export default Matrix;
