3ce611d38903098fd7f9a181d057dfe697b979c5
terryblue99 <terryblue@comcast.net>
2019-05-18 14:32:45 -0500

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import colorSwatches from './colorSwatches'
 
 export default class ColorSelector extends Component {
   
   makeColorSwatches = () => (
-    ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+
+    colorSwatches.map((str, idx) => {
+      // execute the setColor method in Matrix 
+      // when a specific color swatch is clicked
+      // in order to change cells' color
+      const colorChange = () => {this.props.setColor(str)}
+
+      return <div key={idx}
+                  className="color-swatch" 
+                  style={{backgroundColor: str}}
+                  onClick={colorChange} />
     })
   )
   
---
+++ src/colorSwatches.js
@@ -0,0 +1,3 @@
+var colorSwatches = ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"]
+
+export default colorSwatches
