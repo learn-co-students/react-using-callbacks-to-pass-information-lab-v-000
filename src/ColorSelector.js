fc477ab859cfd80523f6ba0500e6c5235e2c42c8
sdcro <sdcrouse91@gmail.com>
2020-07-15 19:17:54 -0700

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
 
+  // callback = (color) => {
+  //   this.props.setSelectedColor(color)
+  // } That won't work; I'd still need to pass the color to it from makeColorSwatches, which then requires onClick={() => {this.callback(str)}}
+  // It would make more sense in that case to just do onClick={() => this.props.setSelectedColor(str)}
+
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      let callback = () => this.props.setSelectedColor(str)
+      return <div onClick={callback} key={idx} className="color-swatch" style={{backgroundColor: str}} />
     })
   )
 
